854b24d52dd3a95bd0ad5aa5d20f7c644c6fa161
Yan de Paula <yan.paula@shipp.delivery>
2021-03-28 19:44:05 -0300

    Relações nas modeis.

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -2,12 +2,18 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\UserFavoriteMovie;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Movie extends Model
 {
     use HasFactory;
 
     protected $hidden = ["created_at", "updated_at"];
+
+    public function userFavorites()
+    {
+        return $this->hasMany(UserFavoriteMovie::class);
+    }
 }
--- app/Models/UserFavoriteMovie.php
+++ app/Models/UserFavoriteMovie.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User;
+use App\Models\Movie;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class UserFavoriteMovie extends Model
 {
@@ -18,4 +20,20 @@ class UserFavoriteMovie extends Model
         'user_id',
         'movie_id'
     ];
+
+    /**
+     * Get the movie associated.
+     */
+    public function movie()
+    {
+        return $this->hasOne(Movie::class, 'id', 'movie_id');
+    }
+
+    /**
+     * Get the movie associated.
+     */
+    public function user()
+    {
+        return $this->hasOne(User::class, 'id', 'user_id');
+    }
 }
