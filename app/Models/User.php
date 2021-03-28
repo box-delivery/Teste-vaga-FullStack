6d601db7ec08ae06ccad9fce1f11301a0c4adf0b
Yan de Paula <yan.paula@shipp.delivery>
2021-03-28 00:27:16 -0300

    Acesso aos tokens

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,4 +44,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function accessTokens()
+    {
+        return $this->hasMany('App\Models\OauthAccessToken')->first();
+    }
 }
