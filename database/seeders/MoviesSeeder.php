b3fa07c9591bc39ef67212dbce91e83058fbae6d
Yan de Paula <yan.paula@shipp.delivery>
2021-03-29 16:08:58 -0300

    Ajuste no seeder

--- database/seeders/MoviesSeeder.php
+++ database/seeders/MoviesSeeder.php
@@ -2,9 +2,10 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Http;
 use App\Models\Movie;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Http;
 
 class MoviesSeeder extends Seeder
 {
@@ -16,30 +17,37 @@ class MoviesSeeder extends Seeder
      */
     public function run()
     {
-        $movies = collect([]);
-
-        for ($page = 1; $page <= 5; $page++) {
-            $response = json_decode(Http::get($this->moviesProvider, [
-                "api_key" => env('TMDB_KEY'),
-                "language" => "pt_BR",
-                "page" => $page
-            ])->body());
-
-            foreach ($response->results as $key => $movie) {
-                $movies->push($movie);
+        try {
+            $movies = collect([]);
+            DB::beginTransaction();
+            if (empty(Movie::first())) {
+                for ($page = 1; $page <= 10; $page++) {
+                    $response = json_decode(Http::get($this->moviesProvider, [
+                        "api_key" => env('TMDB_KEY'),
+                        "language" => "pt-BR",
+                        "page" => $page
+                    ])->body());
+        
+                    foreach ($response->results as $key => $movie) {
+                        $movies->push($movie);
+                    }
+                }
+        
+                foreach ($movies as $key => $m) {
+                    $movie = new Movie();
+        
+                    $movie->title = $m->title;
+                    $movie->original_title = $m->original_title;
+                    $movie->overview = $m->overview;
+                    $movie->release_date = isset($m->release_date) ? $m->release_date : null;
+                    $movie->original_language = isset($m->original_language) ? $m->original_language : null;
+        
+                    $movie->save();
+                }
             }
-        }
-
-        foreach ($movies as $key => $m) {
-            $movie = new Movie();
-
-            $movie->title = $m->title;
-            $movie->original_title = $m->original_title;
-            $movie->overview = $m->overview;
-            $movie->release_date = $m->release_date;
-            $movie->original_language = $m->original_language;
-
-            $movie->save();
+            DB::commit();
+        } catch (\Throwable $th) {
+            DB::rollback();
         }
     }
 }
